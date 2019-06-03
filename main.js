18ba15a81e119ef2cd335b9c8bb1394fffb616e8
Peterson <contact@peterson.by>
2019-06-03 16:20:49 +0300

    init git 7/11 lessons

---
+++ main.js
@@ -0,0 +1,46 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    brand: 'Vue Mastery',
+    product: 'Socks',
+    selectedVariant: 0,
+    details: ["80% cotton", "20% polyester", "Gender neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: 'vmSocks-green-onWhite.jpg',
+        variantQuantity: 10
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: 'vmSocks-blue-onWhite.jpg',
+        variantQuantity: 0
+      }
+    ],
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateProduct(index) {
+      this.selectedVariant = index;
+    },
+    delFromCart() {
+      this.cart--;
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
+    }
+  }
+})
